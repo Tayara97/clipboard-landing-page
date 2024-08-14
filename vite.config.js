38b1a32b223598e82dfbd916a66a3ca545b23acc
Tayara97 <abdelrahmantayara21@gmail.com>
2024-08-14 19:24:00 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: "/clipboard-landing-page/",
   plugins: [react()],
 });
